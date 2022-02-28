943f5cb2838bffcd4aab4680b5782f0d417bddb2
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-02-28 12:43:28 -0300

    aula3

---
+++ pythonProject/aula3.py
@@ -0,0 +1,8 @@
+#orientado a identação
+a = int(input('Primeiro valor: '))
+b = int(input('Segundo valor : '))
+
+if a > b:
+    print ('o maior numero é {}' .format(a))
+
+print ('final do programa')
