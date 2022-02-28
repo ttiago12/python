e1bb6a5e194d599926d075358b15fd5cd2ef4114
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-02-28 17:10:13 -0300

    aula4while

--- pythonProject/aula4.py
+++ pythonProject/aula4.py
@@ -22,13 +22,40 @@
 ##verificando numeros primos dentro de um periodo
 ##utilizando o for e colocando o codigo anterior dentro do for
 ##percorre de 0 ao numero A e imprime os primos
-a = int (input('Entre com um valor: '))
-for num in range(a):
-    div = 0
-    for x in range(1,num+1):
-        resto = num % x
-        if resto == 0:
-            div = div + 1  ##outra forma também é : div += 1
-    if div == 2:
-        print(num)
+# a = int (input('Entre com um valor: '))
+# for num in range(a):
+#     div = 0
+#     for x in range(1,num+1):
+#         resto = num % x
+#         if resto == 0:
+#             div = div + 1  ##outra forma também é : div += 1
+#     if div == 2:
+#         print(num)
+
+# ##While imprime de 0 a 10
+# a = 0
+# while a < 10:
+#     print(a)
+#     a +=1
+
+##RESOLUCAO DE CASO DA AULA3
+a = int (input('Primeiro bimestre: '))
+## se o numero for maior que 10 ele requisita novamente o valor a seguir
+##forca o usuario a inserir o numero correto
+while a > 10:
+    a = int(input('Você digitou errado.Digite novamente// Primeiro bimestre: '))
+b = int (input('Segundo bimestre:'))
+while b > 10:
+    b = int(input('Você digitou errado.Digite novamente// Primeiro bimestre: '))
+c = int (input('Terceiro bimestre:'))
+while c > 10:
+    c = int(input('Você digitou errado.Digite novamente// Primeiro bimestre: '))
+d = int (input('Quarto bimestre: '))
+while d > 10:
+    d = int(input('Você digitou errado.Digite novamente// Primeiro bimestre: '))
+media = (a + b + c + d) / 4
+print ('Media:  {}'.format(media))
+
+
+
 
