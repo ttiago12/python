af1c42c5961f8871ceb5d5782fb955666a28ef9b
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-02-28 13:33:27 -0300

    aula3ifelif

--- pythonProject/aula3.py
+++ pythonProject/aula3.py
@@ -1,8 +1,21 @@
-#orientado a identação
-a = int(input('Primeiro valor: '))
-b = int(input('Segundo valor : '))
+#verificando se um numero é par// simbolo = recebe valor // simbolo == compara valor
+a = int(input('Entre com um valor: '))
+resto = a % 2
+if resto == 0:
+    print ('O Numero é par')
+else:
+    print('O Numero é impar')
 
-if a > b:
-    print ('o maior numero é {}' .format(a))
-
-print ('final do programa')
+# #orientado a identação
+# a = int(input('Primeiro valor: '))
+# b = int(input('Segundo valor : '))
+# c = int (input ('Terceiro valor: '))
+#
+# #if: elif:
+# if a > b and a > c:
+#     print ('o maior numero é {}'.format(a))
+# elif b > a and b > c:
+#     print('O maior numero é {}'.format(b))
+# else:
+#     print ('o maior numero é {}'.format(c))
+# print ('final do programa')
