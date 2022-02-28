0c089bfa8615ee924f10aa75fa4636b5aa644447
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-02-28 19:14:52 -0300

    aula5linhas_tupla

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ DioInnovattion
 -2Aula operadores aritmetricos e interação com o usuario
 -3 if elif comparacao
 -4 for while range(gerar numeros aletorios)
+-5 lista tuplas 
---
+++ aula5.py
@@ -0,0 +1,60 @@
+lista = [1, 3, 5, 7]
+lista_animal = ['cachorro','gato','elefante','lobo','arara']
+
+##tupla
+tupla = (1, 10, 12, 13)
+print(tupla)
+#opcao de qual posicao trazer
+#print(tupla[posicao])
+
+## contagem de elementos na tupla ou lista
+print(len(lista))
+
+##conversao
+
+tupla_animal = tuple(lista_animal)
+print(tupla_animal)
+## mostra o valor da posicao [posicao]
+##print (lista[1])
+
+## funcinona com max min
+##print (sum(lista))
+
+# ## validar valor
+# if 'gato' in lista_animal:
+#     print ('existe um gato na lista')
+# else:
+#     print ('não existe um gato na lista')
+
+# ##multiplicar lista
+# nova_lista = lista_animal * 3
+# print(nova_lista)
+
+# ## validar valor , se não existe inserir o valor na lista com o .appendend
+# if 'lobo' in lista_animal:
+#     print ('existe um lobo na lista')
+# else:
+#     print ('não existe um lobo na lista')
+#     lista_animal.append('lobo')
+#     print('INSERIR VALOR COM O .append')
+#     print(lista_animal)
+
+##retirar ultimo valor da lista ou posso colocar a posição .pop[posicao]
+##lista_animal.pop()
+
+## remover atraves do nome
+# lista_animal.remove('elefante')
+# print (lista_animal)
+
+# ##order lista utilizando o .sort
+# lista.sort()
+# lista_animal.sort()
+# print(lista)
+# print(lista_animal)
+# ##ordenar de forma reversa
+# lista_animal.reverse()
+# print(lista_animal)
+
+##TUPLA são imutaveis
+
+
