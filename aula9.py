4190752e0e781eb1f8c66de9d87bd497628228ec
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-03-10 20:50:07 -0300

    Aula_9

--- aula9.py
+++ aula9.py
@@ -1,7 +1,25 @@
 ## so escreve usa o w
 ##atualizar usa o a
 
-def
-arquivo = open('teste.txt', 'a')
-arquivo.write('\nSegunda Linha')
-arquivo.close()
+def escrever_arquivo(texto):
+##também consegue determinar um diretorio
+    diretorio = '/home/tux/Documentos/GitHub/teste.txt'
+    arquivo = open(diretorio, 'w')
+    ## arquivo = open ('arquivo.txt', w) cria dentro da pasta deste arquivo
+    arquivo.write(texto)
+    arquivo.close()
+
+def atualizar_arquivo(texto):
+    arquivo = open ('teste.txt','a')
+    arquivo.write(texto)
+    arquivo.close()
+
+## R realiza a leitura
+def ler_arquivo(nome_arquivo):
+        arquivo = open (nome_arquivo, 'r')
+        texto = arquivo.read()
+        print (texto)
+if __name__ == '__main__':
+    escrever_arquivo('Primeira linha.\n')
+    ##atualizar_arquivo('3° Linha. \n')
+    ##ler_arquivo('teste.txt')
