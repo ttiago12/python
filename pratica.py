f347af0ee1187ff0ff2ec73d89d7ed3212dfb407
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-03-12 22:48:35 -0300

    Create alves.txt

--- pratica.py
+++ pratica.py
@@ -1,10 +1,16 @@
 class File:
-
-
     def criar(nomeArquivo):
         nomeArquivo = input('NOME DO ARQUIVO= ')
         arquivo = open(nomeArquivo,'x')
+        arquivo.close
+    def escrever(texto):
+        nomeArquivo = input('NOME DO ARQUIVO= ')
+
+        arquivo = open(nomeArquivo,'w')
+
 
 file = File()
 
 file.criar()
+
+file.escrever(input())
