25c66ff55372364d2ea0ef56e0d32355f5bfc240
ttiago12 <74989635+ttiago12@users.noreply.github.com>
2022-03-19 11:10:22 -0300

    Update aula11.py

--- aula11.py
+++ aula11.py
@@ -1,13 +1,21 @@
 ##tratamento de execao aula11
 lista = [1,10]
 
+#CONCEITOS DO FINALY - DANDO ERRO OU NAO O TRECHO É EXECUTADO
+print('\\\\\\\ABRINDO ARQUIVO')
+arquivo = open('teste.txt', 'r')
+
 try:
+
+    texto = arquivo.read()
     ##gerando o erro
-    divisao = 10/1
+    divisao = 10/0
     numero = lista[1]
-    x = a
+    #x = a
+    print('\\\\\\\\\FECHANDO ARQUIVO')
+    arquivo.close()
 except ZeroDivisionError:
-    print('Não é possivel realiar uma divisao por zero')
+    print('Não é possivel realizar uma divisao por zero')
 except IndexError:
     print('Erro ao acessar um indice invalida da lista')
 ## bsae mãe , contem todas as exeções
@@ -18,3 +26,10 @@ except BaseException as ex:
 ##tratamento de erro generico
 except:
     print('Erro  desconhecido')
+##Executa quando não houve exceção
+else:
+    print('EXECUTA QUANDO NAO TEM EXCEÇÃO')
+finally:
+    ##sepre executa
+    print('/////FECHANDO ARQUIVO')
+    arquivo.close()
